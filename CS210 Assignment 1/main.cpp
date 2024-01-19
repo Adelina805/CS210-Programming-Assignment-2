85ca4ef54783be76934623d3078c4366cfc08011
adelinamartinez <adelinaballerina1@gmail.com>
2024-01-19 09:41:56 -0800

    test v1.0

--- CS210 Assignment 1/main.cpp
+++ CS210 Assignment 1/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello Adelina! Is it working?" << std::endl;
+    std::cout << "Hello Adelina! Is it working now?" << std::endl;
     return 0;
 }
